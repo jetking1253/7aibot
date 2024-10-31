67c033eff3b99e8525ee5d2e02513e908a31ddfc
jetking1253 <jiang347119954@163.com>
2024-10-31 23:32:35 +0800

    .
    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
+/** @type {import('next').Config} */
+const config = {
+  // 配置内容
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = config
